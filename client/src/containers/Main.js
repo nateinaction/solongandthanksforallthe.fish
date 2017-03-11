be1d7f0e7b3d02889f33b9b845980ec5dd7e3f06
Nate Gay <nate@ohnate.com>
2017-03-10 18:56:27 -0600

    Adding Google Analytics

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
 import '../styles/containers/Main.css';
 import Header from '../components/Header'
 
+import trackingID from '../secrets/ga'
+ReactGA.initialize(trackingID);
+
+const logPageView = () => {
+  ReactGA.set({page: window.location.pathname});
+  ReactGA.pageview(window.location.pathname);
+}
+
 class Main extends Component {
+  componentDidMount() {
+    logPageView()
+  }
+
   render() {
     return (
       <div className="Main">

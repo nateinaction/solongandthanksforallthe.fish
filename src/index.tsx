61bce3e5de0e5e22298d2c7d5691147a55ca9bcf
Nate Gay <email@nategay.me>
2021-06-26 02:53:09 -0500

    Add Content Security Policy

--- src/index.tsx
+++ src/index.tsx
@@ -12,3 +12,25 @@ for (let i = 0; i < sections.length; i++) {
     sections[i]
   );
 }
+
+// Toggle theme dark mode
+function enableDarkMode(enable: boolean) {
+  document.body.classList.toggle("dark-theme", enable);
+}
+
+// On page load, set the theme
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  enableDarkMode(true);
+} else {
+  enableDarkMode(false);
+}
+
+// On system change, update the theme
+window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', event => {
+  if (event.matches) {
+    enableDarkMode(true);
+  } else {
+    enableDarkMode(false);
+  }
+})
